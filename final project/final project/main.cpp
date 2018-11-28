34bd22c68d98cd67342c53dea92dd223baba7abb
Unknown <haotian.guan@uoit.net>
2018-11-28 15:46:45 -0500

    :D

--- final project/final project/main.cpp
+++ final project/final project/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
-
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 
 
@@ -32,6 +33,46 @@ int main()
 	std::cout << "\n";
 	std::cout << "\n";
 	std::cout << "Controls: W - up    S - down    A - left   D - right\n";
+
+	system("pause");
+
+	char map[5][5] = { { 'X', 'X', 'X', 'X', 'X' },
+	{ 'X', 'S', 'S', 'X', 'X' },
+	{ 'X', 'X', 'S', 'X', 'X' },
+	{ 'X', 'X', 'S', 'S', 'X' },
+	{ 'X', 'X', 'X', 'X', 'X' } };
+
+	bool endLoop = false;
+	while (endLoop == false)
+	{
+		int fruitX;
+		int fruitY;
+		bool randLoop = false;
+		while (randLoop == false)
+		{
+			srand(time(NULL));
+			fruitX = rand() % 4;
+			fruitY = rand() % 4;
+
+			if (map[fruitY][fruitX] != 'S')
+				randLoop = true;
+		}
+
+		system("cls");
+
+		for (int ctrA = 0; ctrA < 5; ctrA++)
+		{
+			for (int ctrB = 0; ctrB < 5; ctrB++)
+			{
+				if (ctrA == fruitY && ctrB == fruitX)
+					std::cout << 'F';
+				else
+					std::cout << map[ctrA][ctrB];
+			}
+			std::cout << std::endl;
+		}
+		Sleep(1000);
+	}
 
 
 
